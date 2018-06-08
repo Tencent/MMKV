61b2a357a7b35e222ec55c9dc3176d62372bb384
guoling <guoling@tencent.com>
2018-06-08 18:42:56 +0800

    MultiProcessSharedPreferences

--- Android/MMKV/mmkvdemo/src/main/java/com/tencent/mmkvdemo/BenchMarkBaseService.java
+++ Android/MMKV/mmkvdemo/src/main/java/com/tencent/mmkvdemo/BenchMarkBaseService.java
@@ -2,6 +2,7 @@ package com.tencent.mmkvdemo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -26,6 +27,7 @@ public abstract class BenchMarkBaseService extends Service {
 
     private final int m_loops = 1000;
     private final String MMKV_ID = "benchmark_interprocess";
+    private final String SP_ID = "benchmark_interprocess_sp";
 
     @Override
     public void onCreate() {
@@ -64,6 +66,7 @@ public abstract class BenchMarkBaseService extends Service {
     protected void batchWriteInt(String caller) {
         mmkvBatchWriteInt(caller);
         sqliteWriteInt(caller);
+        spBatchWriteInt(caller);
     }
     private void mmkvBatchWriteInt(String caller) {
         Random r = new Random();
@@ -93,10 +96,27 @@ public abstract class BenchMarkBaseService extends Service {
         long endTime = System.currentTimeMillis();
         System.out.println(caller + " sqlite write int: loop[" + m_loops + "]: " + (endTime - startTime) + "ms");
     }
+    private void spBatchWriteInt(String caller) {
+        Random r = new Random();
+        long startTime = System.currentTimeMillis();
+
+        SharedPreferences preferences = MultiProcessSharedPreferences.getSharedPreferences(this, SP_ID, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        for (int index = 0; index < m_loops; index++) {
+            int tmp = r.nextInt();
+            String key = m_arrIntKeys[index];
+            editor.putInt(key, tmp);
+//            editor.commit();
+            editor.apply();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(caller + " MultiProcessSharedPreferences write int: loop[" + m_loops +"]: " + (endTime-startTime) + "ms");
+    }
 
     protected void batchReadInt(String caller) {
         mmkvBatchReadInt(caller);
         sqliteReadInt(caller);
+        spBatchReadInt(caller);
     }
     private void mmkvBatchReadInt(String caller) {
         long startTime = System.currentTimeMillis();
@@ -122,10 +142,22 @@ public abstract class BenchMarkBaseService extends Service {
         long endTime = System.currentTimeMillis();
         System.out.println(caller + " sqlite read int: loop[" + m_loops + "]: " + (endTime - startTime) + "ms");
     }
+    private void spBatchReadInt(String caller) {
+        long startTime = System.currentTimeMillis();
+
+        SharedPreferences preferences = MultiProcessSharedPreferences.getSharedPreferences(this, SP_ID, MODE_PRIVATE);
+        for (int index = 0; index < m_loops; index++) {
+            String key = m_arrIntKeys[index];
+            int tmp = preferences.getInt(key, 0);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(caller + " MultiProcessSharedPreferences read int: loop[" + m_loops +"]: " + (endTime-startTime) + "ms");
+    }
 
     protected void batchWriteString(String caller) {
         mmkvBatchWriteString(caller);
         sqliteWriteString(caller);
+        spBatchWrieString(caller);
     }
     private void mmkvBatchWriteString(String caller) {
         long startTime = System.currentTimeMillis();
@@ -153,10 +185,26 @@ public abstract class BenchMarkBaseService extends Service {
         long endTime = System.currentTimeMillis();
         System.out.println(caller + " sqlite write String: loop[" + m_loops + "]: " + (endTime - startTime) + "ms");
     }
+    private void spBatchWrieString(String caller) {
+        long startTime = System.currentTimeMillis();
+
+        SharedPreferences preferences = MultiProcessSharedPreferences.getSharedPreferences(this, SP_ID, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        for (int index = 0; index < m_loops; index++) {
+            final String str = m_arrStrings[index];
+            final String key = m_arrKeys[index];
+            editor.putString(key, str);
+//            editor.commit();
+            editor.apply();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(caller + " MultiProcessSharedPreferences write String: loop[" + m_loops +"]: " + (endTime-startTime) + "ms");
+    }
 
     protected void batchReadString(String caller) {
         mmkvBatchReadString(caller);
         sqliteReadString(caller);
+        spBatchReadStrinfg(caller);
     }
     private void mmkvBatchReadString(String caller) {
         long startTime = System.currentTimeMillis();
@@ -181,6 +229,17 @@ public abstract class BenchMarkBaseService extends Service {
 //        sqlIteKV.endTransaction();
         long endTime = System.currentTimeMillis();
         System.out.println(caller + " sqlite read String: loop[" + m_loops + "]: " + (endTime - startTime) + "ms");
+    }
+    private void spBatchReadStrinfg(String caller) {
+        long startTime = System.currentTimeMillis();
+
+        SharedPreferences preferences = MultiProcessSharedPreferences.getSharedPreferences(this, SP_ID, MODE_PRIVATE);
+        for (int index = 0; index < m_loops; index++) {
+            final String key = m_arrKeys[index];
+            final String tmp = preferences.getString(key, null);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(caller + " MultiProcessSharedPreferences read String: loop[" + m_loops +"]: " + (endTime-startTime) + "ms");
     }
 
     @Nullable
