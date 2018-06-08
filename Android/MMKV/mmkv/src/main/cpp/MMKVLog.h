aebfef318b91c4e85df6f156562a212f63be190d
guoling <guoling@tencent.com>
2018-06-08 17:04:50 +0800

    logging switch

--- Android/MMKV/mmkv/src/main/cpp/MMKVLog.h
+++ Android/MMKV/mmkv/src/main/cpp/MMKVLog.h
@@ -9,16 +9,25 @@
 #include <cstring>
 #include <errno.h>
 
+// enable logging
+#define ENABLE_MMKV_LOG
+
+#ifdef ENABLE_MMKV_LOG
+
 #define APPNAME "MMKV"
 
-//#define MMKVError(format, ...)	    __android_log_print(ANDROID_LOG_ERROR, APPNAME, format, ##__VA_ARGS__)
-//#define MMKVWarning(format, ...)	__android_log_print(ANDROID_LOG_WARN, APPNAME, format, ##__VA_ARGS__)
-//#define MMKVInfo(format, ...)		__android_log_print(ANDROID_LOG_INFO, APPNAME, format, ##__VA_ARGS__)
-//#define MMKDebug(format, ...)	    __android_log_print(ANDROID_LOG_DEBUG, APPNAME, format, ##__VA_ARGS__)
+#define MMKVError(format, ...)	    __android_log_print(ANDROID_LOG_ERROR, APPNAME, format, ##__VA_ARGS__)
+#define MMKVWarning(format, ...)	__android_log_print(ANDROID_LOG_WARN, APPNAME, format, ##__VA_ARGS__)
+#define MMKVInfo(format, ...)		__android_log_print(ANDROID_LOG_INFO, APPNAME, format, ##__VA_ARGS__)
+#define MMKDebug(format, ...)	    __android_log_print(ANDROID_LOG_DEBUG, APPNAME, format, ##__VA_ARGS__)
+
+#else
 
 #define MMKVError(format, ...)	    {}
 #define MMKVWarning(format, ...)	{}
 #define MMKVInfo(format, ...)		{}
 #define MMKDebug(format, ...)	    {}
+
+#endif
 
 #endif //MMKV_MMKVLOG_H
