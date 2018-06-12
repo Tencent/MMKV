ad4b9d4d81adac93a870cf1867d048bc7cb443e6
guoling <guoling@tencent.com>
2018-06-12 12:30:50 +0800

    import tidy

--- Android/MMKV/mmkvdemo/src/main/java/com/tencent/mmkvdemo/MainActivity.java
+++ Android/MMKV/mmkvdemo/src/main/java/com/tencent/mmkvdemo/MainActivity.java
@@ -2,19 +2,16 @@ package com.tencent.mmkvdemo;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.tencent.mmkv.MMKV;
-
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Random;
+
+import com.tencent.mmkv.MMKV;
 
 public class MainActivity extends AppCompatActivity {
     @Override
