ef88e2df5946f3140b8533859df2c848b77d63f2
guoling <guoling@tencent.com>
2018-07-13 20:53:59 +0800

    iOS performance Test

--- iOS/MMKVDemo/MMKVDemoTests/MMKVDemoTests.mm
+++ iOS/MMKVDemo/MMKVDemoTests/MMKVDemoTests.mm
@@ -221,11 +221,4 @@ using namespace std;
 	XCTAssertEqualObjects(sValue, @"hello");
 }
 
-- (void)testPerformanceExample {
-	// This is an example of a performance test case.
-	[self measureBlock:^{
-	    // Put the code you want to measure the time of here.
-	}];
-}
-
 @end
