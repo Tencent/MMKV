0a8a5c3861184bf774eb1a60c944ef5a9f91bbbc
guoling <guoling@tencent.com>
2018-07-16 13:49:15 +0800

    Licensed header

--- iOS/MMKV/MMKV/MiniCodedInputData.mm
+++ iOS/MMKV/MMKV/MiniCodedInputData.mm
@@ -1,10 +1,22 @@
-//
-//  MiniCodedInputData.mm
-//  MicroMessenger
-//
-//  Created by Guo Ling on 4/26/13.
-//  Copyright (c) 2013 Tencent. All rights reserved.
-//
+/*
+ * Tencent is pleased to support the open source community by making
+ * WCDB available.
+ *
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *       https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "MiniCodedInputData.h"
 #import "MiniPBUtility.h"
--- iOS/MMKV/MMKV/MiniCodedOutputData.mm
+++ iOS/MMKV/MMKV/MiniCodedOutputData.mm
@@ -1,10 +1,22 @@
-//
-//  MiniCodedOutputData.mm
-//  StorageProfile
-//
-//  Created by Ling Guo on 4/17/14.
-//  Copyright (c) 2014 Tencent. All rights reserved.
-//
+/*
+ * Tencent is pleased to support the open source community by making
+ * WCDB available.
+ *
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *       https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "MiniCodedOutputData.h"
 #import "MiniPBUtility.h"
