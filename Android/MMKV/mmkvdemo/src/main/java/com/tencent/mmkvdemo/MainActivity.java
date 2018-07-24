361f235f709ddd734251f49f44036ba4f59073a2
guoling <guoling@tencent.com>
2018-07-24 17:10:39 +0800

    codesige debug

--- Android/MMKV/mmkvdemo/src/main/java/com/tencent/mmkvdemo/MainActivity.java
+++ Android/MMKV/mmkvdemo/src/main/java/com/tencent/mmkvdemo/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         testMMKV();
-        //testAshmem();
+        testAshmem();
         //testInterProcessLock();
         //estImportSharedPreferences();
     }
