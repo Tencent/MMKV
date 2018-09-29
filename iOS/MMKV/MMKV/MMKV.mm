71940b965cae028502bad3b508fdb6f8f7fd40c7
zhongwuzw <zhongwuzw@qq.com>
2018-09-29 23:15:29 +0800

    Small optimize by removing redundant code

--- iOS/MMKV/MMKV/MMKV.mm
+++ iOS/MMKV/MMKV/MMKV.mm
@@ -69,13 +69,10 @@ static NSRecursiveLock *g_instanceLock;
 
 + (void)initialize {
 	if (self == MMKV.class) {
-		static dispatch_once_t onceToken;
-		dispatch_once(&onceToken, ^{
-			g_instanceDic = [NSMutableDictionary dictionary];
-			g_instanceLock = [[NSRecursiveLock alloc] init];
-
-			MMKVInfo(@"pagesize:%d", DEFAULT_MMAP_SIZE);
-		});
+		g_instanceDic = [NSMutableDictionary dictionary];
+		g_instanceLock = [[NSRecursiveLock alloc] init];
+		
+		MMKVInfo(@"pagesize:%d", DEFAULT_MMAP_SIZE);
 	}
 }
 
@@ -257,7 +254,7 @@ NSData *decryptBuffer(AESCrypt &crypter, NSData *inputBuffer) {
 						if (m_cryptor) {
 							inputBuffer = decryptBuffer(*m_cryptor, inputBuffer);
 						}
-						m_dic = [MiniPBCoder decodeContainerOfClass:NSMutableDictionary.class withValueClass:NSData.class fromData:inputBuffer];
+						m_dic = [MiniPBCoder decodeContainerOfClass:NSDictionary.class withValueClass:NSData.class fromData:inputBuffer];
 						m_output = new MiniCodedOutputData(m_ptr + offset + m_actualSize, m_size - offset - m_actualSize);
 					} else {
 						[self writeAcutalSize:0];
