0178111d93cc2c83419ad432f863dd8b91413c55
guoling <guoling@tencent.com>
2019-02-25 21:35:22 +0800

    indent

--- iOS/MMKV/MMKV/aes/openssl/md5_dgst.cpp
+++ iOS/MMKV/MMKV/aes/openssl/md5_dgst.cpp
@@ -37,7 +37,7 @@ int MD5_Init(MD5_CTX *c)
 void md5_block_data_order(MD5_CTX *c, const void *data_, size_t num)
 {
     auto data = (const unsigned char *) data_;
-	unsigned MD32_REG_T A, B, C, D, l;
+    unsigned MD32_REG_T A, B, C, D, l;
 # ifndef MD32_XARRAY
     /* See comment in crypto/sha/sha_locl.h for details. */
     unsigned MD32_REG_T XX0, XX1, XX2, XX3, XX4, XX5, XX6, XX7,
