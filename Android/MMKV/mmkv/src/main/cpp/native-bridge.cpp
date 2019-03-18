59fbd86f0f044d1744a40966eeab06c1adfde66e
xwy <aerialladder@163.com>
2019-03-18 13:33:51 +0800

    fix decodebytes bug

--- Android/MMKV/mmkv/src/main/cpp/native-bridge.cpp
+++ Android/MMKV/mmkv/src/main/cpp/native-bridge.cpp
@@ -477,8 +477,8 @@ extern "C" JNIEXPORT JNICALL jbyteArray Java_com_tencent_mmkv_MMKV_decodeBytes(J
     if (kv && oKey) {
         string key = jstring2string(env, oKey);
         MMBuffer value = kv->getBytesForKey(key);
-        jbyteArray result = env->NewByteArray(value.length());
-        if (result) {
+        if (value.length() > 0){
+            jbyteArray result = env->NewByteArray(value.length());
             env->SetByteArrayRegion(result, 0, value.length(), (const jbyte *) value.getPtr());
             return result;
         }
