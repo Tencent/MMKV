2bc96c478283f6c8e4cb8b1851a61546414bc0b8
guoling <guoling@tencent.com>
2020-04-15 12:22:05 +0800

    update file header copyright year

--- iOS/MMKVDemo/WatchApp Extension/ExtensionDelegate.h
+++ iOS/MMKVDemo/WatchApp Extension/ExtensionDelegate.h
@@ -2,7 +2,7 @@
  * Tencent is pleased to support the open source community by making
  * MMKV available.
  *
- * Copyright (C) 2019 THL A29 Limited, a Tencent company.
+ * Copyright (C) 2020 THL A29 Limited, a Tencent company.
  * All rights reserved.
  *
  * Licensed under the BSD 3-Clause License (the "License"); you may not use
