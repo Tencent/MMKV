115fd54912a039e2ef069106dc3661b389c1707b
guoling <guoling@tencent.com>
2020-11-17 17:58:21 +0800

    await MMKV initialize() on main()

--- flutter/example/lib/main.dart
+++ flutter/example/lib/main.dart
@@ -23,7 +23,12 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:MMKVFlutter/mmkv.dart';
 
-void main() {
+void main() async {
+
+  // must wait for MMKV to finish initialization
+  final rootDir = await MMKV.initialize();
+  print('MMKV for flutter with rootDir = $rootDir');
+
   runApp(MyApp());
 }
 
@@ -42,10 +47,6 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-
-    final rootDir = await MMKV.initialize();
-    print('rootDir = $rootDir');
-
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
