429e9303d2d5facd9ab037ef52fef2b57dee28a1
Kyle <kyle201817146@gmail.com>
2022-04-10 14:08:52 +0800

    Add BCSymbolMaps support

--- iOS/archive_xcframework.sh
+++ iOS/archive_xcframework.sh
@@ -32,6 +32,8 @@ xcodebuild \
 -create-xcframework \
 -framework archives/MMKV-iOS.xcarchive/Products/Library/Frameworks/MMKV.framework \
 -debug-symbols $PWD/archives/MMKV-iOS.xcarchive/dSYMs/MMKV.framework.dSYM \
+-debug-symbols $PWD/archives/MMKV-iOS.xcarchive/BCSymbolMaps/0909AADB-3F01-3418-B7F2-E2B9FB6E315F.bcsymbolmap \
+-debug-symbols $PWD/archives/MMKV-iOS.xcarchive/BCSymbolMaps/441383DA-0478-34F4-9A38-F2521EB5B045.bcsymbolmap \
 -framework archives/MMKV-iOS-Simulator.xcarchive/Products/Library/Frameworks/MMKV.framework \
 -debug-symbols $PWD/archives/MMKV-iOS-Simulator.xcarchive/dSYMs/MMKV.framework.dSYM \
 -framework archives/MMKV-macOS.xcarchive/Products/Library/Frameworks/MMKV.framework \
