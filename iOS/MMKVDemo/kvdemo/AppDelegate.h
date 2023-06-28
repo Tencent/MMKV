980c4137c710173850c5b860e5b319b25320f1ab
guoling <guoling@tencent.com>
2023-06-28 21:44:24 +0800

    add cross process demo for iOS

---
+++ iOS/MMKVDemo/kvdemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kvdemo
+//
+//  Created by lingol on 2023/6/28.
+//  Copyright © 2023 Lingol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
