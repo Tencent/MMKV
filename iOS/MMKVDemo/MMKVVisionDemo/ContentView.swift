d63393b36c03f83e334590ddf8b7c27eb8ded1fe
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2024-02-16 09:12:19 +0100

    chore: add VisionDemo target

---
+++ iOS/MMKVDemo/MMKVVisionDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MMKVVisionDemo
+//
+//  Created by Cyril Bonaccini on 15/02/2024.
+//  Copyright © 2024 Lingol. All rights reserved.
+//
+
+import SwiftUI
+import MMKV
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ iOS/MMKVDemo/MMKVVisionDemo/MMKVVisionDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  MMKVVisionDemoApp.swift
+//  MMKVVisionDemo
+//
+//  Created by Cyril Bonaccini on 15/02/2024.
+//  Copyright © 2024 Lingol. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MMKVVisionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
