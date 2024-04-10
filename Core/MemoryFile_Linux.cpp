5a70ee4e84e3a5b984b4a4f58c4f49a52a2a87fe
guoling <guoling@tencent.com>
2024-04-10 21:42:56 +0800

    OHOS doesnt have renameat2() eventhough the macro SYS_renameat2 is defined

--- Core/MemoryFile_Linux.cpp
+++ Core/MemoryFile_Linux.cpp
@@ -54,7 +54,7 @@ bool tryAtomicRename(const MMKVPath_t &srcPath, const MMKVPath_t &dstPath) {
     bool renamed = false;
 
     // try renameat2() first
-#ifdef SYS_renameat2
+#if defined(SYS_renameat2) && !defined(MMKV_OHOS)
 #ifdef MMKV_ANDROID
     static auto g_renameat2 = (renameat2_t) dlsym(RTLD_DEFAULT, "renameat2");
     if (g_renameat2) {
@@ -72,7 +72,7 @@ bool tryAtomicRename(const MMKVPath_t &srcPath, const MMKVPath_t &dstPath) {
         MMKVWarning("fail on syscall(SYS_renameat2) [%s] to [%s], %d(%s)", srcPath.c_str(), dstPath.c_str(), errno,
                     strerror(errno));
     }
-#endif // SYS_renameat2
+#endif // SYS_renameat2 && !MMKV_OHOS
 
     if (!renamed) {
         if (::rename(srcPath.c_str(), dstPath.c_str()) != 0) {
