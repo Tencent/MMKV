0a967e53bf24c36b8d69c03185977bc497867fe9
chenjindong <chenjindong@52tt.com>
2025-05-26 11:45:29 +0800

    flutter: remove Android's v1 embedding

--- flutter/mmkv_android/android/src/main/java/com/tencent/mmkv/MMKVPlugin.java
+++ flutter/mmkv_android/android/src/main/java/com/tencent/mmkv/MMKVPlugin.java
@@ -27,7 +27,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** MmkvPlugin */
 public class MMKVPlugin implements FlutterPlugin, MethodCallHandler {
