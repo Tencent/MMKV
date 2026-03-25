e8168d323a28e39d8349bcb9bf57e4312d0bfb30
guoling <guoling@tencent.com>
2026-03-25 15:58:11 +0800

    tidy ohos project config

--- OpenHarmony/hvigorfile.ts
+++ OpenHarmony/hvigorfile.ts
@@ -1,6 +1,17 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
+import signData from './sign-config.json';
 
 export default {
-    system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+    system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+    plugins: [], /* Custom plugin to extend the functionality of Hvigor. */
+    config: {
+        ohos: {
+            overrides: {
+                signingConfig: {
+                    type: "HarmonyOS",
+                    material: signData,
+                }
+            }
+        }
+    }
 }
